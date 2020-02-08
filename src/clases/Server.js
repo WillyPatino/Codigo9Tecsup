9183ab6412e6baae643dfb2a59512afa4f1a5978
Willy Patiño <30963719+Hackalmighty@users.noreply.github.com>
2020-02-07 20:03:00 -0500

    Mi commit XD

--- src/clases/Server.js
+++ src/clases/Server.js
@@ -31,7 +31,7 @@ class Server {
   cargarRutas() {
     // Asignando todas las rutas de pabellon_router al servidor
     this.app.get('/',(req,res)=>{
-      res.status(200).send('La API funcional!!!');
+      res.status(200).send('La API funciona 😂 😂 😂!!!');
     })
     this.app.use('/', pabellon_router, ambiente_router, reserva_router, usuario_router);
   }
